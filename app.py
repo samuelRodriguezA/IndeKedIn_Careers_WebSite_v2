2595cdab4857284f4bc520d3564e768f6f7f2b5a
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-01-26 21:47:21 +0000

    Conection To Db

--- app.py
+++ app.py
@@ -1,36 +1,21 @@
-from flask import Flask,render_template, jsonify
+from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-JOBS = [
-  {
-    'id':1,
-    'title':'Intern wealthsimple, Software Development (Summer 2024)',
-    'location':'Toronto',
-    'salary':'$50,000'
-  },
-  {
-    'id':2,
-    'title':'Snowflake, SOFTWARE ENGINEER INTERN (TORONTO) - SUMMER 2024',
-    'location':'Toronto',
-    'salary':'$50,000'
-  },
-  {
-    'id':3,
-    'title':'Velocity - Software Engineer Internship/Co-Op - Summer 2024',
-    'location':'Toronto',
-    'salary':'$50,000'
-  }
-    ]
+
+
 @app.route("/")
-def hello_world():
-  return render_template("home.html",
-                         jobs=JOBS,
-                        company_name="IndeKedIn")
+def hello_indekedin():
+    jobs = load_jobs_from_db()
+    return render_template("home.html",
+                           jobs=jobs,
+                           company_name="IndeKedIn")
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
-  
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
