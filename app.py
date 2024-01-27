bf9e77ff323c6e7c8a725fdef31fb9a2b187a61a
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-01-27 19:44:39 +0000

    Upload data from form to DB

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db,add_application_to_db
 
 app = Flask(__name__)
 
@@ -25,6 +25,16 @@ def show_job(id):
         return "Not Found", 404
     return render_template('jobpage.html', job=job)
     
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submited.html', 
+                           application=data,
+                           job=job,
+                           job_id=id
+                        )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine, text
 import os
+
+from sqlalchemy import create_engine, text
+
 db_connection_string = os.environ['DB_CONNNECTION_STRING']
 
 engine = create_engine(db_connection_string, connect_args={
@@ -24,3 +26,20 @@ def load_job_from_db(id):
       return None
     else:
       return dict(rows[0]._asdict())
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+      query = text("INSERT INTO applications (job_id, full_name, email,linkedin_url, github_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :github_url, :education, :work_experience, :resume_url)")
+      conn.execute(query, {
+          "job_id": job_id,
+          "full_name": data['full_name'],
+          "email": data['email'],
+          "linkedin_url": data['linkedin_url'],
+          "github_url": data['github_url'],
+          "education": data['education'],
+          "work_experience": data['work_experience'],
+          "resume_url": data['resume_url']
+      })
+
+
+
