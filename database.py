c5a874c418fa9407447653a2ee8fb6ab3cc4981f
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-01-27 15:17:38 +0000

    Added Dinamic Pages for each job

--- database.py
+++ database.py
@@ -13,3 +13,14 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs"))
     jobs = [dict(row._asdict()) for row in result]
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from jobs where id = :id"),{"id": id}
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._asdict())
